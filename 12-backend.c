5f863e2eb99e4da3b523f4ef97cf90af2fc425e9
Praveen007bot <54658995+Praveen007bot@users.noreply.github.com>
2022-09-23 11:32:32 +0530

    Add files via upload

---
+++ 12-backend.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main()
+{
+    char icode[10][30], str[20], opr[10];
+    int i = 0;
+    printf("Enter the set of intermediate code (terminate by 'exit'): ");
+    do
+    {
+        scanf("%s", icode[i]);
+    } while (strcmp(icode[i++], "exit") != 0);
+    printf("\nTarget code generation");
+    printf("\n*********************");
+    i = 0;
+    do
+    {
+        strcpy(str, icode[i]);
+        switch (str[3])
+        {
+        case '+':
+            strcpy(opr, "ADD");
+            break;
+        case '-':
+            strcpy(opr, "SUB");
+            break;
+        case '*':
+            strcpy(opr, "MUL");
+            break;
+        case '/':
+            strcpy(opr, "DIV");
+            break;
+
+        default:
+            break;
+        }
+        printf("\n\tMOV %c,R%d", str[2],i);
+        printf("\n\t%s%c,R%d", opr, str[4], i);
+        printf("\n\tMOV R%d,%c", i, str[0]);
+    } while (strcmp(icode[i++], "exit") != 0);
+}
